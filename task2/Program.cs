4b1e91d98fbe945d7bcab075ff545922b31c54be
Igor Leontyev <crybzleon@yandex.ru>
2023-03-12 20:31:03 +0300

    sem9

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Input M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+int Recurs(int M,int N)
+{
+    if (N == M)
+    {
+        return N;
+    }
+    return N + Recurs(M,N-1);
+}
+
+Console.Write($"{Recurs(M,N)}");
---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Acer(int m, int n)
+{
+    if (m > 0 && n == 0)
+    {
+        return Acer(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return Acer(m - 1, Acer(m, n - 1));
+    }
+    return n + 1;
+}
+Console.Write($"{Acer(m, n)}");
